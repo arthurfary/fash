d130c49d7e75721b0d80e7f1c8eb655378ac67fb
Vinicius Nadolny <vinirnadolny@gmail.com>
2026-06-04 12:56:26 -0300

    Implement base window manager with grid structure

--- fash/windowmanager/window.py
+++ fash/windowmanager/window.py
@@ -1,14 +1,28 @@
 class Window:
     pass
 
-    def __init__(self):
-        pass
+    def __init__(self, row: int, col: int):
+        self.grid = [[]]
+        self.make_grid(row, col)
 
-    def make_grid(self, row, col):
-        pass
+    def make_grid(self, row: int, col: int):
+        row_list = []
+        for i in range(row):
+            col_list = []
+            for j in range(col):
+                col_list.append(None) # TODO: base widget class? specific widget for blank window in grid?
+            row_list.append(col_list)
+        
+        self.grid = row_list
 
-    def get_at(self, row, col):
-        pass
+    def get_at(self, row: int, col: int):
+        if (row >= len(self.grid)) or (col >= len(self.grid[row])):
+            raise IndexError
+        
+        return self.grid[row][col]
 
-    def set_at(self, row, col, widget):
-        pass
+    def set_at(self, row: int, col: int, widget):
+        if (row >= len(self.grid)) or (col >= len(self.grid[row])):
+            raise IndexError
+        
+        self.grid[row][col] = widget
